adc7591469c83c189ac5f42f84b760c9ddb4ea72
logro <lo.grosjean1@gmail.com>
2025-11-28 10:01:13 +0100

    init commit

---
+++ src/main/java/org/example/Personne.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Personne {
+    private String identifiant;
+    private String nom;
+    private String adresse;
+    private String contact;
+
+    public Personne(String identifiant, String nom, String adresse, String contact){
+        this.identifiant = identifiant;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.contact = contact;
+    }
+
+    public String getIdentifiant(){
+        return identifiant;
+    }
+    public String getNom(){
+        return nom;
+    }
+    public String getAdresse(){
+        return adresse;
+    }
+    public String getContact(){
+        return contact;
+    }
+
+    public void ObtenirInfos(){
+        System.out.println("Nom :" + nom + "Identifiant : " + identifiant + "adresse" + adresse + "Contact : " + contact);
+    }
+}
