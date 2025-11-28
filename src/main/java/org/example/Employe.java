b9cee8daffcc14d30ea5022c1a09b69ad2492b4f
logro <lo.grosjean1@gmail.com>
2025-11-28 11:19:47 +0100

    init commit

--- src/main/java/org/example/Employe.java
+++ src/main/java/org/example/Employe.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import java.util.Date;
+
 public class Employe extends Personne {
+    private int NumeroEmploye;
+    private String DateEmbauche;
+
+    public Employe(int NumeroEmploye, String DateEmbauche){
+        super(NumeroEmploye, DateEmbauche);
+    }
+    public int getNumeroEmploye(){
+        return NumeroEmploye;
+    }
+
+    public String getDateEmbauche(){
+        return DateEmbauche;
+    }
+
+    @Override
+    public String ObtenirRole(identifiant identifiant){
+        return "C'est un (role) ";
+    }
 }
