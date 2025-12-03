3d50c84c7ee467a4abcc8b07ab5a3a7398fc64a5
AlleyB0ss <165383614+AlleyB0ss@users.noreply.github.com>
2025-12-03 19:13:55 +0100

    Update Avion.java

--- src/main/java/Nathanielle/Avion.java
+++ src/main/java/Nathanielle/Avion.java
@@ -1,4 +1,48 @@
 package Nathanielle;
+package compagnieAerienne;
 
 public class Avion {
+
+    private String immatriculation;
+    private String modele;
+    private int capacite;
+
+    public Avion(String immatriculation, String modele, int capacite) {
+        this.immatriculation = immatriculation;
+        this.modele = modele;
+        this.capacite = capacite;
+    }
+
+    public String getImmatriculation() {
+        return immatriculation;
+    }
+
+    public void setImmatriculation(String immatriculation) {
+        this.immatriculation = immatriculation;
+    }
+
+    public String getModele() {
+        return modele;
+    }
+
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
+    public int getCapacite() {
+        return capacite;
+    }
+
+    public void setCapacite(int capacite) {
+        this.capacite = capacite;
+    }
+
+    public boolean verifierDisponibilite() {
+        // suite pour voir si l'avion est free à un mmt
+        return true;
+    }
+
+    public void affecterVol(Vol vol) {
+        // avion = vol
+    }
 }
