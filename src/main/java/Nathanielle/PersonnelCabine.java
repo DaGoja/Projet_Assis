6ade5e9ae53217ef86c67a90975752c32a6f9024
AlleyB0ss <165383614+AlleyB0ss@users.noreply.github.com>
2025-12-03 19:12:01 +0100

    Extend PersonnelCabine class from Employe

--- src/main/java/Nathanielle/PersonnelCabine.java
+++ src/main/java/Nathanielle/PersonnelCabine.java
@@ -1,4 +1,26 @@
 package Nathanielle;
 
-public class PersonnelCabine {
+public class PersonnelCabine extends Employe {
+
+    private String qualification;
+
+    public PersonnelCabine(int identifiant, String nom, String adresse, String contact,
+                           int numeroEmploye, String dateEmbauche,
+                           String qualification) {
+        super(identifiant, nom, adresse, contact, numeroEmploye, dateEmbauche);
+        this.qualification = qualification;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    @Override
+    public String obtenirRole() {
+        return "Personnel cabine";
+    }
 }
