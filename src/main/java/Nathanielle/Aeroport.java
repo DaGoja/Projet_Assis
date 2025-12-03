c65d02057bf003cb302dafc5d5f8b20bc768e101
AlleyB0ss <165383614+AlleyB0ss@users.noreply.github.com>
2025-12-03 19:15:03 +0100

    Add Aeroport class with properties and methods

--- src/main/java/Nathanielle/Aeroport.java
+++ src/main/java/Nathanielle/Aeroport.java
@@ -1,4 +1,30 @@
 package Nathanielle;
 
 public class Aeroport {
+
+    private String nom;
+    private String ville;
+    private String description;
+
+    public Aeroport(String nom, String ville, String description) {
+        this.nom = nom;
+        this.ville = ville;
+        this.description = description;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void affecterVol(Vol vol) {
+        // ajouter vol à la liste des vols
+    }
 }
