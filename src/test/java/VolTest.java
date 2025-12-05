aa532180a83fda08e50a7686cfaea0216af0cbce
AlleyB0ss <165383614+AlleyB0ss@users.noreply.github.com>
2025-12-05 12:17:06 +0100

    Add test for listing active passengers in Vol

--- src/test/java/VolTest.java
+++ src/test/java/VolTest.java
@@ -1,2 +1,30 @@
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class VolTest {
+
+    @Test
+    void testListingPassagerIgnoreReservationsAnnulees() {
+        Aeroport cdg = new Aeroport("CDG", "Paris", "");
+        Aeroport fco = new Aeroport("FCO", "Rome", "");
+        Vol vol = new Vol("TO350", cdg, fco,
+                "25/12/2024 10:15", "", "PLANIFIE");
+
+        Passager p1 = new Passager("P1", "Durand", "Adresse", "0600000000", "AA1");
+        Passager p2 = new Passager("P2", "Martin", "Adresse", "0600000001", "BB2");
+
+        // Deux réservations
+        Reservation r1 = p1.reserverVol(vol, "R1", "20/12/2024");
+        Reservation r2 = p2.reserverVol(vol, "R2", "20/12/2024");
+
+        // On annule la seconde réservation
+        r2.annulerReservation();
+
+        // On récupère la liste des passagers actifs
+        List<Passager> actifs = vol.listingPassager();
+
+        assertEquals(1, actifs.size());
+        assertEquals("Durand", actifs.get(0).getNom());
+    }
 }
