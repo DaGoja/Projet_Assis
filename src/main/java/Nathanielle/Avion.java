9505af5602e895ba8b6f3f602ce71b44ffba2bb8
AlleyB0ss <165383614+AlleyB0ss@users.noreply.github.com>
2025-12-05 10:01:11 +0100

    Update Avion.java

--- src/main/java/Nathanielle/Avion.java
+++ src/main/java/Nathanielle/Avion.java
@@ -1,4 +1,4 @@
-package Nathanielle;
+import java.util.ArrayList;
 
 public class Avion {
 
@@ -6,11 +6,46 @@ public class Avion {
     private String modele;
     private int capacite;
 
+    private ArrayList<Vol> listeVols = new ArrayList<>();
+    private static ArrayList<Avion> flotte = new ArrayList<>();
+
     public Avion(String immatriculation, String modele, int capacite) {
         this.immatriculation = immatriculation;
         this.modele = modele;
         this.capacite = capacite;
+        flotte.add(this);
     }
+
+    public void affecterVol(Vol vol) {
+        if (!listeVols.contains(vol)) {
+            listeVols.add(vol);
+            vol.setAvion(this);
+        }
+    }
+
+    /**
+     * Version très simple : on retourne toujours true.
+     * Plus tard tu pourras vérifier les dates de vol.
+     */
+    public boolean verifierDisponibilite() {
+        return true;
+    }
+
+    // CRUD basique sur la flotte
+    public static Avion chercherParImmatriculation(String immat) {
+        for (Avion a : flotte) {
+            if (a.immatriculation.equals(immat)) return a;
+        }
+        return null;
+    }
+
+    public static boolean supprimerParImmatriculation(String immat) {
+        Avion a = chercherParImmatriculation(immat);
+        if (a != null) return flotte.remove(a);
+        return false;
+    }
+
+    // Getters / setters
 
     public String getImmatriculation() {
         return immatriculation;
@@ -36,12 +71,11 @@ public class Avion {
         this.capacite = capacite;
     }
 
-    public boolean verifierDisponibilite() {
-        // suite pour voir si l'avion est free à un mmt
-        return true;
+    public ArrayList<Vol> getListeVols() {
+        return listeVols;
     }
 
-    public void affecterVol(Vol vol) {
-        // avion = vol
+    public static ArrayList<Avion> getFlotte() {
+        return flotte;
     }
 }
