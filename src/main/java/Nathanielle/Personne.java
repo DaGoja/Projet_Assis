30e939e214a7d85089116ee5ea39b212f98f3334
AlleyB0ss <165383614+AlleyB0ss@users.noreply.github.com>
2025-12-05 09:58:24 +0100

    Implement CRUD operations for Personne class
    Added CRUD functionality for managing a list of Personne objects.

--- src/main/java/Nathanielle/Personne.java
+++ src/main/java/Nathanielle/Personne.java
@@ -1,30 +1,98 @@
-package Nathanielle;
+import java.util.ArrayList;
 
 public class Personne {
+
+    // Attributs de base d'une personne
     private String identifiant;
     private String nom;
     private String adresse;
     private String contact;
 
-    public Personne(String identifiant, String nom, String adresse, String contact){
+    // Petite "base" en mémoire pour faire du CRUD
+    private static ArrayList<Personne> listePersonnes = new ArrayList<>();
+
+    public Personne(String identifiant, String nom, String adresse, String contact) {
         this.identifiant = identifiant;
         this.nom = nom;
         this.adresse = adresse;
         this.contact = contact;
+
+        // à chaque création on ajoute la personne dans la liste
+        listePersonnes.add(this);
     }
 
-    public String getIdentifiant(){return identifiant;}
-    public String getNom(){
+    /**
+     * Affiche les infos de la personne (décrit dans l’énoncé).
+     */
+    public void obtenirInfos() {
+        System.out.println(this);
+    }
+
+    // ---- CRUD très simple sur la liste statique ----
+
+    public static Personne chercherParIdentifiant(String id) {
+        for (Personne p : listePersonnes) {
+            if (p.identifiant.equals(id)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public static boolean supprimerParIdentifiant(String id) {
+        Personne p = chercherParIdentifiant(id);
+        if (p != null) {
+            return listePersonnes.remove(p);
+        }
+        return false;
+    }
+
+    public static ArrayList<Personne> getListePersonnes() {
+        return listePersonnes;
+    }
+
+    // toString pour avoir un affichage propre
+    @Override
+    public String toString() {
+        return "Personne{" +
+                "id='" + identifiant + '\'' +
+                ", nom='" + nom + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", contact='" + contact + '\'' +
+                '}';
+    }
+
+    // Getters / setters (classiques)
+
+    public String getIdentifiant() {
+        return identifiant;
+    }
+
+    public void setIdentifiant(String identifiant) {
+        this.identifiant = identifiant;
+    }
+
+    public String getNom() {
         return nom;
     }
-    public String getAdresse(){
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
         return adresse;
     }
-    public String getContact(){
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getContact() {
         return contact;
     }
 
-    public void ObtenirInfos(){
-        System.out.println("Nom :" + nom + "Identifiant : " + identifiant + "adresse" + adresse + "Contact : " + contact);
+    public void setContact(String contact) {
+        this.contact = contact;
     }
 }
