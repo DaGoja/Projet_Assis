fa9a948a65683a7685f823cc704802e107237461
AlleyB0ss <165383614+AlleyB0ss@users.noreply.github.com>
2025-12-05 10:00:17 +0100

    Refactor Passager class and add reservation methods
    Refactor Passager class to improve structure and add reservation management.

--- src/main/java/Nathanielle/Passager.java
+++ src/main/java/Nathanielle/Passager.java
@@ -1,14 +1,50 @@
-package Nathanielle;
+import java.util.ArrayList;
 
-public class Passager extends Personne{
-    private String Passeport;
+public class Passager extends Personne {
 
-    public Passager(identifiant, nom, adresse, contact){
+    private String passeport;
+    private ArrayList<Reservation> listeReservations = new ArrayList<>();
+
+    public Passager(String identifiant, String nom, String adresse, String contact,
+                    String passeport) {
         super(identifiant, nom, adresse, contact);
-        this.Passeport = Passeport;
+        this.passeport = passeport;
     }
 
-    public String getPasseport(){
-        return
+    /**
+     * Le passager réserve un vol.
+     * Ici je passe le numéro de réservation et la date en paramètre,
+     * mais tu peux adapter plus tard.
+     */
+    public Reservation reserverVol(Vol vol, String numeroReservation, String dateReservation) {
+        Reservation r = new Reservation(numeroReservation, dateReservation,
+                "EN_ATTENTE", this, vol);
+
+        listeReservations.add(r);
+        vol.ajouterReservation(r);
+
+        return r;
+    }
+
+    public boolean annulerReservation(String numeroReservation) {
+        for (Reservation r : listeReservations) {
+            if (r.getNumeroReservation().equals(numeroReservation)) {
+                r.annulerReservation();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<Reservation> obtenirReservations() {
+        return listeReservations;
+    }
+
+    public String getPasseport() {
+        return passeport;
+    }
+
+    public void setPasseport(String passeport) {
+        this.passeport = passeport;
     }
 }
