b25b2f463dce9127add101b08a235eae69bf0ac9
AlleyB0ss <165383614+AlleyB0ss@users.noreply.github.com>
2025-12-07 17:19:46 +0100

    Refactor VolTest to add testCreationVol method

--- src/test/java/VolTest.java
+++ src/test/java/VolTest.java
@@ -1,30 +1,15 @@
 import org.junit.jupiter.api.Test;
-import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class VolTest {
 
     @Test
-    void testListingPassagerIgnoreReservationsAnnulees() {
-        Aeroport cdg = new Aeroport("CDG", "Paris", "");
-        Aeroport fco = new Aeroport("FCO", "Rome", "");
-        Vol vol = new Vol("TO350", cdg, fco,
-                "25/12/2024 10:15", "", "PLANIFIE");
+    public void testCreationVol() {
+        Aeroport a = new Aeroport("CDG", "Paris", "");
+        Aeroport b = new Aeroport("FCO", "Rome", "");
 
-        Passager p1 = new Passager("P1", "Durand", "Adresse", "0600000000", "AA1");
-        Passager p2 = new Passager("P2", "Martin", "Adresse", "0600000001", "BB2");
+        Vol v = new Vol("T1", a, b, "d1", "d2", "PLANIFIE");
 
-        // Deux réservations
-        Reservation r1 = p1.reserverVol(vol, "R1", "20/12/2024");
-        Reservation r2 = p2.reserverVol(vol, "R2", "20/12/2024");
-
-        // On annule la seconde réservation
-        r2.annulerReservation();
-
-        // On récupère la liste des passagers actifs
-        List<Passager> actifs = vol.listingPassager();
-
-        assertEquals(1, actifs.size());
-        assertEquals("Durand", actifs.get(0).getNom());
+        assertEquals("T1", v.getNumeroVol());
     }
 }
