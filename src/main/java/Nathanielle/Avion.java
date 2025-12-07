a35dbf0c3098a53bf7bd4714244bdd29d7a3a560
AlleyB0ss <165383614+AlleyB0ss@users.noreply.github.com>
2025-12-07 12:49:02 +0100

    Refactor Avion class to rename listeVols
    Renamed 'listeVols' to 'vols' and updated related methods.

--- src/main/java/Nathanielle/Avion.java
+++ src/main/java/Nathanielle/Avion.java
@@ -6,7 +6,7 @@ public class Avion {
     private String modele;
     private int capacite;
 
-    private ArrayList<Vol> listeVols = new ArrayList<>();
+    private ArrayList<Vol> vols = new ArrayList<>();
     private static ArrayList<Avion> flotte = new ArrayList<>();
 
     public Avion(String immatriculation, String modele, int capacite) {
@@ -17,21 +17,16 @@ public class Avion {
     }
 
     public void affecterVol(Vol vol) {
-        if (!listeVols.contains(vol)) {
-            listeVols.add(vol);
+        if (!vols.contains(vol)) {
+            vols.add(vol);
             vol.setAvion(this);
         }
     }
 
-    /**
-     * Version très simple : on retourne toujours true.
-     * Plus tard tu pourras vérifier les dates de vol.
-     */
     public boolean verifierDisponibilite() {
         return true;
     }
 
-    // CRUD basique sur la flotte
     public static Avion chercherParImmatriculation(String immat) {
         for (Avion a : flotte) {
             if (a.immatriculation.equals(immat)) return a;
@@ -45,37 +40,7 @@ public class Avion {
         return false;
     }
 
-    // Getters / setters
-
     public String getImmatriculation() {
         return immatriculation;
-    }
-
-    public void setImmatriculation(String immatriculation) {
-        this.immatriculation = immatriculation;
-    }
-
-    public String getModele() {
-        return modele;
-    }
-
-    public void setModele(String modele) {
-        this.modele = modele;
-    }
-
-    public int getCapacite() {
-        return capacite;
-    }
-
-    public void setCapacite(int capacite) {
-        this.capacite = capacite;
-    }
-
-    public ArrayList<Vol> getListeVols() {
-        return listeVols;
-    }
-
-    public static ArrayList<Avion> getFlotte() {
-        return flotte;
     }
 }
