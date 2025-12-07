5d946a506cac4221147878ec6dd000788776dc94
AlleyB0ss <165383614+AlleyB0ss@users.noreply.github.com>
2025-12-07 17:19:21 +0100

    Add test for saving reservations without exceptions

--- src/test/java/GestionFichiersCompagnieTest.java
+++ src/test/java/GestionFichiersCompagnieTest.java
@@ -4,7 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GestionFichiersCompagnieTest {
 
     @Test
-    void testImportFlightsNePlantePas() {
-        assertDoesNotThrow(() -> GestionFichiersCompagnie.importFlights());
+    public void testSauvegarderReservationsNePlantePas() {
+        Aeroport a = new Aeroport("CDG", "Paris", "");
+        Aeroport b = new Aeroport("FCO", "Rome", "");
+        Vol v = new Vol("T3", a, b, "d1", "d2", "PLANIFIE");
+
+        Passager p = new Passager("P1", "Durand", "Lyon", "0600", "AA111");
+        Reservation r = p.reserverVol(v, "R1", "20/12/2024");
+        r.confirmerReservation();
+
+        assertDoesNotThrow(() -> {
+            GestionFichiersCompagnie.sauvegarderReservations("reservations_test.csv");
+        });
     }
 }
